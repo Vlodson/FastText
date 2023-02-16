274ce6653aea4e1689035a232c8d87a394cb09e5
Vladimir Lunić <vladimir.lunic2000@gmail.com>
2023-02-16 12:30:56 +0100

    Fix errors in __main__

--- source/__main__.py
+++ source/__main__.py
@@ -1,21 +1,11 @@
-from model import train
-from text_preprocesssing import preprocess_corpus
-from text_node_with_keyword import make_node_word_list, keywords_similarity, keyword_in_node
+from .text_node_with_keyword import node_has_keywords
 
 
 def main():
-    ngram_map, context_map, ngram_vectors, word_vectors, word_map = preprocess_corpus(
-        "../texts/NM_train-corpus-short.txt"
-    )
+    with open("../texts/NM_test-corpus.txt", "r", encoding="utf-8") as f:
+        text = f.read()
 
-    vector_space = train(word_map, False)
-    node_words = make_node_word_list("../texts/NM_test-corpus.txt")
-
-    keyword_similarities = keywords_similarity(
-        ["lorem", "pera", "consectetum"], node_words, ngram_vectors, vector_space
-    )
-
-    print(keyword_in_node(keyword_similarities, 0.1))
+    print(node_has_keywords(text, ["lorem", "pera", "consectetum"]))
 
 
 if __name__ == "__main__":
