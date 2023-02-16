a0257b0f293582b04ac51415b150474d1b1f1344
Vladimir Lunić <vladimir.lunic2000@gmail.com>
2023-02-16 11:30:51 +0100

    Merge keyword in node functions into one function for ease of use

--- source/text_node_with_keyword.py
+++ source/text_node_with_keyword.py
@@ -1,19 +1,25 @@
 import numpy as np
+import os
 
 from typing import List, Dict, Tuple
 
-from text_preprocesssing import clean_text, make_word_list, remove_stopwords
-from embedding import embed_word
-from similarity import vector_cosine_similarity
-from io_utils import open_corpus
-from globals import STOPWORDS_PATH
+from .text_preprocesssing import clean_text, make_word_list, remove_stopwords
+from .embedding import embed_word
+from .similarity import vector_cosine_similarity
+from .io_utils import deserialize_object
+from .globals import (
+    STOPWORDS_PATH,
+    ROOT,
+    SAVE_DIR,
+    VECTOR_SPACE_FILE,
+    NGRAM_VECTORS_FILE,
+)
 
 
-def make_node_word_list(node_path: str) -> List[str]:
+def _make_node_word_list(text: str) -> List[str]:
     """
     Makes a list of words from the text node that needs to be processed
     """
-    text = open_corpus(node_path)
     text = clean_text(text)
     word_list = make_word_list(text)
     word_list = remove_stopwords(word_list, STOPWORDS_PATH)
@@ -21,8 +27,12 @@ def make_node_word_list(node_path: str) -> List[str]:
     return word_list
 
 
-def keywords_similarity(keywords: List[str], node_word_list: List[str], ngram_vectors: Dict[str, np.ndarray],
-                        vector_space: np.ndarray) -> Dict[str, Dict[str, float]]:
+def _keywords_similarity(
+    keywords: List[str],
+    node_word_list: List[str],
+    ngram_vectors: Dict[str, np.ndarray],
+    vector_space: np.ndarray,
+) -> Dict[str, Dict[str, float]]:
     """
     Embeds each word in the node word list and each keyword.
     Then for each embedded keyword finds the similarity between each of the words in the node word list.
@@ -34,35 +44,66 @@ def keywords_similarity(keywords: List[str], node_word_list: List[str], ngram_ve
     # this removes all the ones that couldn't be embedded in this context
     embedded_keywords = {
         keyword: embed_word(keyword, ngram_vectors, vector_space)
-        for keyword in keywords if type(embed_word(keyword, ngram_vectors, vector_space)) != int
+        for keyword in keywords
+        if type(embed_word(keyword, ngram_vectors, vector_space)) != int
     }
 
     embedded_node_words = {
         word: embed_word(word, ngram_vectors, vector_space)
-        for word in node_word_list if type(embed_word(word, ngram_vectors, vector_space)) != int
+        for word in node_word_list
+        if type(embed_word(word, ngram_vectors, vector_space)) != int
     }
 
     keyword_similarities = {
-        keyword:
-            {
-                word: vector_cosine_similarity(embedded_keyword, embedded_word)
-                for word, embedded_word in embedded_node_words.items()
-            }
+        keyword: {
+            word: vector_cosine_similarity(embedded_keyword, embedded_word)
+            for word, embedded_word in embedded_node_words.items()
+        }
         for keyword, embedded_keyword in embedded_keywords.items()
     }
 
     return keyword_similarities
 
 
-def keyword_in_node(keyword_similarities: Dict[str, Dict[str, float]],
-                    threshold: float = 0.1) -> List[Tuple[str, bool]]:
+def _keyword_in_node(
+    keyword_similarities: Dict[str, Dict[str, float]], threshold: float = 0.1
+) -> List[Tuple[str, bool]]:
     """
     Based on the keyword similarities dict and a similarity threshold,
     returns a list of tuples with the keyword and whether the node has that keyword in it
     """
     # if the similarity is over 0.1 for at least one word in a keyword similarity dict then mark that one as True
     keyword_nodes = [
-        (keyword, sum([value >= threshold for value in keyword_similarities[keyword].values()]) >= 1)
+        (
+            keyword,
+            sum(
+                [value >= threshold for value in keyword_similarities[keyword].values()]
+            )
+            >= 1,
+        )
         for keyword in keyword_similarities
     ]
     return keyword_nodes
+
+
+def node_has_keywords(node_text: str, keywords: List[str]) -> List[Tuple[str, bool]]:
+    """
+    Based on node content and users keywords, marks whether the node has any keywords.
+
+    Returns a list of tuples with the keyword and whether the node has that keyword in it
+    """
+    project_path = os.path.join(ROOT, SAVE_DIR)
+    ngram_vectors: Dict[str, np.ndarray] = deserialize_object(
+        os.path.join(project_path, NGRAM_VECTORS_FILE)
+    )
+    vector_space: np.ndarray = deserialize_object(
+        os.path.join(project_path, VECTOR_SPACE_FILE)
+    )
+
+    node_word_list = _make_node_word_list(node_text)
+    keyword_similarities = _keywords_similarity(
+        keywords, node_word_list, ngram_vectors, vector_space
+    )
+    keyword_in_node = _keyword_in_node(keyword_similarities)
+
+    return keyword_in_node
