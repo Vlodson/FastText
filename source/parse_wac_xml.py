4841507be9a4562d96fd821cfda1ce0d41ad58c3
Vladimir Lunić <vladimir.lunic2000@gmail.com>
2023-02-17 19:21:08 +0100

    Add script to parse xml files with corpus

---
+++ source/parse_wac_xml.py
@@ -0,0 +1,42 @@
+from bs4 import Tag
+from bs4 import BeautifulSoup
+import re
+
+
+def parse_body(body: Tag) -> str:
+    text = body.contents[0]
+    text = re.sub(r"(\s+|\n+|\t+|\r+)", ' ', text)
+
+    words = text.split(' ')[1:-1]
+    first_col_words = [word for word in words[::4]]
+    remade = ""
+    for word in first_col_words:
+        remade += word + ' '
+
+    return remade
+
+
+def parse_xml(xml_path: str) -> str:
+    with open(xml_path, 'r', encoding='utf-8') as f:
+        data = f.read()
+
+    xml_data = BeautifulSoup(data, 'xml')
+    bodies = xml_data.find_all('s')
+
+    parsed_bodies = [parse_body(body) for body in bodies]
+
+    corpus = ""
+    for body in parsed_bodies:
+        corpus += body + '\n'
+
+    return corpus
+
+
+def main():
+    corpus = parse_xml("../../WaC/srWaC1.1.06_short.xml")
+    with open('../texts/serbian_corpus_150k.txt', 'w', encoding='utf-8') as f:
+        f.write(corpus)
+
+
+if __name__ == '__main__':
+    main()
